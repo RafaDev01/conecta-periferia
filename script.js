696694d069c3b2b504055928c43178519fe7d540
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2023-11-20 12:26:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+setTimeout(()=>{
+    alert("Testando se o script ta funcionando...")
+    alert("Comecarei pela versão mobile ok, precisará ser acessado pelo celular para melhor experiência até eu tirar esse alerta.")
+},2000)
