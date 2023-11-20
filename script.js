e1a211d13cfc12aae33f3461f2b4e1ae6d23643c
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2023-11-20 14:21:03 -0300

    menu pronto

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+/*
 setTimeout(()=>{
     alert("Testando se o script ta funcionando...")
     alert("Comecarei pela versão mobile ok, precisará ser acessado pelo celular para melhor experiência até eu tirar esse alerta.")
 },2000)
+*/
+
+const btnMobile = document.querySelector('#btn-mobile')
+
+const toggleMenu=(event)=>{
+    if(event.type === 'touchstart'){
+        event.preventDefault()
+    }
+
+    const nav = document.querySelector("#nav")
+    nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener("click",(event)=>{
+    toggleMenu(event)
+})
+
+btnMobile.addEventListener("touchstart",(event)=>{
+    toggleMenu(event)
+})
