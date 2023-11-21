dca4067bea6e4b8483e3c5d3aa2391c810ca4830
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2023-11-21 00:17:57 -0300

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
+/*
 setTimeout(()=>{
     alert("Testando se o script ta funcionando...")
     alert("Comecarei pela versão mobile ok, precisará ser acessado pelo celular para melhor experiência até eu tirar esse alerta.")
 },2000)
-
+*/
 
 const btnMobile = document.querySelector('#btn-mobile')
 
